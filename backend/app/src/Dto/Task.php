7aa1e31ffaed7b38a8899fc23f782731d380251f
Manol Trendafilov <manol@camplight.dev>
2025-03-22 10:49:56 +0200

    add task dto

---
+++ backend/app/src/Dto/Task.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Dto;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Task {
+    public function __construct(
+        #[Assert\NotBlank]
+        #[Assert\Length(min: 3, max: 255)]
+        public string $title,
+        #[Assert\NotBlank]
+        #[Assert\Length(min: 3, max: 255)]
+        public string $description,
+        #[Assert\NotBlank]
+        public bool $completed,
+        #[Assert\NotBlank]
+        public string $created_at,
+        #[Assert\NotBlank]
+        public string $updated_at
+    ) {}
+}
