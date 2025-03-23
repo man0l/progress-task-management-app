735f0816644f050627d2100ecc43d7f4af6f859c
Manol Trendafilov <manol@camplight.dev>
2025-03-23 11:44:55 +0200

    add dashboard widgets

--- backend/app/src/Controller/DashboardController.php
+++ backend/app/src/Controller/DashboardController.php
@@ -5,14 +5,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Contracts\Api\TaskInterface;
 
 final class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
-    public function index(): Response
+    public function index(TaskInterface $taskApi): Response
     {
+        $tasks = $taskApi->getTasks();
+        $completedTasks = array_filter($tasks, function($task) {
+            return $task->completed;
+        });
+        $pendingTasks = array_filter($tasks, function($task) {
+            return !$task->completed;
+        });
+
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
+            'completedTasks' => count($completedTasks),
+            'pendingTasks' => count($pendingTasks)
         ]);
     }
 }
