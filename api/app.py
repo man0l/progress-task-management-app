99db7786c51bbfaca6ef1b4a23016df5370fde3a
Manol Trendafilov <manol@camplight.dev>
2025-03-23 18:01:16 +0200

    add endpoints for users

--- api/app.py
+++ api/app.py
@@ -185,15 +185,6 @@ def delete_task(task_id):
 
     return '', 200
 
-@app.route("/users", methods=["GET"])
-@jwt_required()
-@role_required(["admin", "user"])
-def get_users():
-    return jsonify(users=[{
-        'id': user.id,
-        'email': user.email
-    } for user in db_session.query(User).all()])
-
 @app.route("/tasks/<int:task_id>/assign", methods=["POST"])
 @jwt_required()
 @role_required(["admin", "user"])
@@ -225,6 +216,75 @@ def assign_task(task_id):
         'created_at': task.created_at.isoformat(),
         'updated_at': task.updated_at.isoformat()
     })
+
+@app.route("/users", methods=["GET"])
+@jwt_required()
+@role_required(["admin", "user"])
+def get_users():
+    return jsonify(users=[{
+        'id': user.id,
+        'email': user.email
+    } for user in db_session.query(User).all()])
+
+@app.route("/users/<int:user_id>", methods=["GET"])
+@jwt_required()
+@role_required(["admin", "user"])
+def get_user(user_id):
+    user = db_session.query(User).filter_by(id=user_id).first()
+    return jsonify({
+        'id': user.id,
+        'email': user.email
+    })
+
+@app.route("/users", methods=["POST"])
+@jwt_required()
+@role_required(["admin", "user"])
+def create_user():
+    data = request.json
+    if not data or 'email' not in data:
+        return jsonify({"msg": "Email is required"}), 400
+
+    if not data or 'password' not in data:
+        return jsonify({"msg": "Password is required"}), 400
+
+    user = User(email=data['email'], password=data['password'])
+    db_session.add(user)
+    db_session.commit()
+
+    return jsonify({
+        'id': user.id,
+        'email': user.email
+    })
+
+@app.route("/users/<int:user_id>", methods=["PUT"])
+@jwt_required()
+@role_required(["admin", "user"])
+def update_user(user_id):
+    data = request.json
+    if not data or 'email' not in data:
+        return jsonify({"msg": "Email is required"}), 400
+
+    user = db_session.query(User).filter_by(id=user_id).first()
+    if not user:
+        return jsonify({"msg": "User not found"}), 404
+
+    user.email = data['email']
+    db_session.commit()
+
+    return jsonify({
+        'id': user.id,
+        'email': user.email
+    })
+
+@app.route("/users/<int:user_id>", methods=["DELETE"])
+@jwt_required()
+@role_required(["admin"])
+def delete_user(user_id):
+    user = db_session.query(User).filter_by(id=user_id).first()
+    db_session.delete(user)
+    db_session.commit()
+
+    return '', 200
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
